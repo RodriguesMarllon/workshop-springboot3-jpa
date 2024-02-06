edd001f82fcf15dcb62f8caea852709fbf5f30e1
Desktop Marllon <marllonrsantos@gmail.com>
2024-02-06 19:15:38 -0300

    Feat: JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/javacourseudemy/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.javacourseudemy.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.javacourseudemy.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+}

882be4121ca09c6e9cf8b73a884a7cc9ae9615e0
Desktop Marllon <marllonrsantos@gmail.com>
2024-02-07 22:23:06 -0300

    Feat: Product

---
+++ src/main/java/com/javacourseudemy/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.javacourseudemy.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javacourseudemy.course.entities.Product;
+import com.javacourseudemy.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj =  repository.findById(id);
+		return obj.get();
+	}
+}

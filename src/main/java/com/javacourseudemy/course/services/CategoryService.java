8863d479c1c398827a90d9df6263f50e79bd72a6
Desktop Marllon <marllonrsantos@gmail.com>
2024-02-07 22:05:39 -0300

    Feat: Category

---
+++ src/main/java/com/javacourseudemy/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.javacourseudemy.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javacourseudemy.course.entities.Category;
+import com.javacourseudemy.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj =  repository.findById(id);
+		return obj.get();
+	}
+}

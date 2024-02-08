fb1183dab9ace6a3c7278a9ce1bab861ca1f32f8
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-02-08 10:27:11 -0300

    Feat: OrderItem, many-to-many association with extra attributes (Part 2)

---
+++ src/main/java/com/javacourseudemy/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.javacourseudemy.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.javacourseudemy.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+	
+}

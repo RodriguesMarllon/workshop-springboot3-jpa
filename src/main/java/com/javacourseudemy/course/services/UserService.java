4bb741e1a0b820bd5a88878eedcdd150af0d9ee1
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-02-09 09:32:44 -0300

    Feat: Exception handling - findById

--- src/main/java/com/javacourseudemy/course/services/UserService.java
+++ src/main/java/com/javacourseudemy/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.javacourseudemy.course.entities.User;
 import com.javacourseudemy.course.repositories.UserRepository;
+import com.javacourseudemy.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj =  repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
